b8fbf0ac1535df042fc6a9711f25760445ab1900
Viktor <krpectv57@gmail.com>
2020-10-06 19:47:42 +0200

    EndsWith

--- Open-Lab-03.08/Checker.cs
+++ Open-Lab-03.08/Checker.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._08
     {
         public bool IsPlural(string word)
         {
-            throw new NotImplementedException();
+            if (word.EndsWith("s"))
+                return true;
+            else
+                return false;
         }
     }
 }
